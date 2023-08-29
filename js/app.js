8d88b5c96781771c7fd54b9f5b8baf4221591e7a
Najoua Bentaher <117578236+NajouaBentaher@users.noreply.github.com>
2023-08-29 11:04:33 +0100

    Auto Save & Auto Load

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
+// declare vaiables 
+var htmlV = document.getElementById("htmlCode")
+var cssV = document.getElementById("cssCode");
+var jsV = document.getElementById("jsCode");
+(function setValues(){
+if(localStorage.getItem("code")){
+    var code = JSON.parse(localStorage.getItem("code"))
+    htmlV.value=code.htmlC;
+    cssV.value=code.cssC;
+    jsV.value=code.jsC;
+}else
+{
 document.getElementById("htmlCode").value="<div>\n\n</div>";
 document.getElementById("cssCode").value="<style>\n\n</style>";
 document.getElementById("jsCode").value="<script>\n\n</script>";
-
+}
+showPreview()
+})()
 function showPreview(){
     var htmlCode = document.getElementById("htmlCode").value;
     var cssCode = ""+document.getElementById("cssCode").value+"";
     var jsCode = ""+document.getElementById("jsCode").value+"";
+   
+    // save codes in an object 
+    var codeObj = {
+        htmlC:htmlCode,
+        cssC:cssCode,
+        jsC:jsCode
+    }
+    // save the code in local storage
+    localStorage.setItem("code",JSON.stringify(codeObj))
+
+
     var frame = document.getElementById("preview-window").contentWindow.document;
     frame.open();
     frame.write(htmlCode+cssCode+jsCode);
